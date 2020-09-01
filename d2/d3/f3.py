9d4ffd03f324776b75ca6105c9e7c0cf5a0c999f
IanMcConnel <38693554+IanMcConnel@users.noreply.github.com>
2020-09-01 13:21:04 -0500

    Update f3.py

--- d2/d3/f3.py
+++ d2/d3/f3.py
@@ -1,6 +1,5 @@
-dsddsdsdsd
-dsfsdfsfdfsd
-dsfsdsdfsdfsd
-sdfsdfsdfsdfsd
-vv boots2 z266
-  bbbb ccccc   zz
+First step in my surgical journey: small tests to determine my health for more advanced tests Friday. 
+Did anyone else know they now require 15 ml of spit for the COVID test? For the record, that is a ton of saliva. 
+And the woman ahead of me is nailing it. 
+One step at a time ... 
+(Apologies for the pic of my spit).
